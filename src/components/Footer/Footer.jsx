d795b5b8d8d5f92622088dac23faeec4beb80621
unknown <ipriyanshugautam@gmail.com>
2022-01-12 23:31:42 +0530

    SpeedCheck

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <p
+                className="footer-link"
+            >
+                Keep Typing!!!
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
